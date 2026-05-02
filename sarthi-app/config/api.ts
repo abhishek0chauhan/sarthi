69a6fabb147d97ae43d05facd2ad60b85899b910
abhishek-chauhan-git <abhishek.chauhan@acldigital.com>
2026-05-02 16:40:32 +0530

    feat: install socket.io-client and add WS_BASE config

--- sarthi-app/config/api.ts
+++ sarthi-app/config/api.ts
@@ -2,4 +2,8 @@ export const API_BASE = __DEV__
   ? 'http://192.168.1.2:3000'
   : 'https://api.sarthi.app';
 
+export const WS_BASE = __DEV__
+  ? 'http://192.168.1.2:3000'
+  : 'wss://api.sarthi.app';
+
 export const API_TIMEOUT_MS = 300000;
